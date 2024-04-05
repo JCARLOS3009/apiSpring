e15008621c62d171da67f2c01487f31d74db5b6c
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-04-05 18:19:09 -0300

    Add files via upload

---
+++ src/test/java/hedgbuild/com/demo/ApispringApplicationTests.java
@@ -0,0 +1,13 @@
+package hedgbuild.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
